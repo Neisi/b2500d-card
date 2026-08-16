9c683e197bc0823e431c5ba500dbabe1219797e7
Yunus Emre Umar <77045015+emre155@users.noreply.github.com>
2026-08-16 18:24:18 +0300

    chore: update dist bundle for .options and @closed on ha-select

--- dist/b2500d-card.js
+++ dist/b2500d-card.js
@@ -1130,16 +1130,15 @@ class B2500DCard extends i {
                   <div class="right">
                     <ha-select
                       .value=${entity.state}
+                      .options=${entity.attributes?.options || []}
+                      @closed=${(e) => e.stopPropagation()}
                       @change=${(e) => {
-                        const val = e.target.value;
+                        const val = e.detail?.value ?? e.target.value;
                         this._hass.callService("select", "select_option", {
                           entity_id: entity.entity_id,
                           option: val
                         });
                       }}>
-                      ${(entity.attributes?.options || []).map(
-                        (opt) => b`<mwc-list-item value=${opt}>${opt}</mwc-list-item>`
-                      )}
                     </ha-select>
                   </div>
                 </div>
